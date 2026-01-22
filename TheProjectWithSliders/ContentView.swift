b1c1ef8b8e60388d380c370f1f3ab1880c079976
AtesiT <atesit@mail.ru>
2026-01-22 17:37:07 +0300

    ADD: button(it used when somebody wants to input data)

--- TheProjectWithSliders/ContentView.swift
+++ TheProjectWithSliders/ContentView.swift
@@ -4,18 +4,36 @@ struct ContentView: View {
     
     @State private var sliderValue = Double.random(in: 0...255)
     @State private var userName = ""
+    @State private var displayedName = ""
+    @State private var isPresented: Bool = false
     
     var body: some View {
-        VStack {
+        VStack(spacing: 40) {
             Text(lround(sliderValue).formatted())
                 .font(.largeTitle)
-            UserNameView(name: userName)
+            UserNameView(name: displayedName)
             ColorSliderView(sliderValue: $sliderValue)
             TextField("Enter your name", text: $userName)
                 .textFieldStyle(.roundedBorder)
+            Button("Done", action: checkUserName)
+                .alert("Wrong format", isPresented: $isPresented, actions: {}) {
+                    Text("Enter your name")
+                }
+
             Spacer()
         }
         .padding()
+    }
+    
+    private func checkUserName() {
+        if let _ = Double(userName) {
+            userName = ""
+            isPresented.toggle()
+            return
+        }
+        
+        displayedName = userName
+        userName = ""
     }
 }
 
@@ -25,7 +43,7 @@ struct UserNameView : View {
     var body: some View {
         HStack {
             HStack(alignment: .firstTextBaseline) {
-                Text("USER NAME: ")
+                Text("USER NAME: ").frame(height: 60)
                 Text(name)
                     .font(.largeTitle)
             }
