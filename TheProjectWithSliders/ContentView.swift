9eff50f1101084d0ad450cb0a01547505c9049af
AtesiT <atesit@mail.ru>
2026-01-22 14:38:13 +0300

    Initial Commit

---
+++ TheProjectWithSliders/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .font(.largeTitle)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
