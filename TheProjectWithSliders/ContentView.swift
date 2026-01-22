34558b351deb25ec5afcd5c81d6a1c93f053eb75
AtesiT <atesit@mail.ru>
2026-01-22 15:49:21 +0300

    ADDED: TextField with UIKIt style and HStack in subview

--- TheProjectWithSliders/ContentView.swift
+++ TheProjectWithSliders/ContentView.swift
@@ -3,20 +3,31 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sliderValue = Double.random(in: 0...255)
+    @State private var userName = ""
     
     var body: some View {
         VStack {
             Text(lround(sliderValue).formatted())
                 .font(.largeTitle)
-            HStack {
-                Text("0").foregroundStyle(.red)
-                Slider(value: $sliderValue, in: 0...255, step: 1)
-                Text("255").foregroundStyle(.red)
-            }
-            
+            ColorSliderView(sliderValue: $sliderValue)
+            TextField("Enter your name", text: $userName)
+                .textFieldStyle(.roundedBorder)
             Spacer()
         }
         .padding()
+    }
+}
+
+struct ColorSliderView : View {
+    
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundStyle(.red)
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+            Text("255").foregroundStyle(.red)
+        }
     }
 }
 
