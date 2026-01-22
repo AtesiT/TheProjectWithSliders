e0a11de246d389094cbe1ec7088a28450fa2a8ae
AtesiT <atesit@mail.ru>
2026-01-22 15:08:58 +0300

    MADE: added slider(it uses int)

--- TheProjectWithSliders/ContentView.swift
+++ TheProjectWithSliders/ContentView.swift
@@ -1,10 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sliderValue = Double.random(in: 0...255)
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            Text(lround(sliderValue).formatted())
                 .font(.largeTitle)
+            HStack {
+                Text("0").foregroundStyle(.red)
+                Slider(value: $sliderValue, in: 0...255, step: 1)
+                Text("255").foregroundStyle(.red)
+            }
+            
             Spacer()
         }
         .padding()
