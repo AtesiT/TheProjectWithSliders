e95dd7b26bf2661b1d7f242146fe5c8c4a0643e6
AtesiT <atesit@mail.ru>
2026-01-24 17:18:50 +0300

    MADE: was added button on the right up of the keyboard

--- TheProjectWithSliders/ViewAndThreeSliders.swift
+++ TheProjectWithSliders/ViewAndThreeSliders.swift
@@ -2,11 +2,13 @@ import SwiftUI
 
 struct ViewAndThreeSliders: View {
     
-    @State var redValue = Double.random(in: 0...255).rounded()
-    @State var greenValue = Double.random(in: 0...255).rounded()
-    @State var blueValue = Double.random(in: 0...255).rounded()
+    @State private var redValue = Double.random(in: 0...255).rounded()
+    @State private var greenValue = Double.random(in: 0...255).rounded()
+    @State private var blueValue = Double.random(in: 0...255).rounded()
+    
     var colorView: Color { Color(red: redValue/255, green: greenValue/255, blue: blueValue/255) }
     
+    @FocusState private var isInputActive: Bool
     
     var body: some View {
         ZStack {
@@ -14,11 +16,21 @@ struct ViewAndThreeSliders: View {
                 .ignoresSafeArea()
             VStack {
                 ColorView(width: 365, height: 135, color: colorView, cornerRadius: 25)
-                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 50, trailing: 0))
+                    .frame(height: 200)
                 VStack {
                     SliderView(color: .red, sliderValue: $redValue)
                     SliderView(color: .green, sliderValue: $greenValue)
                     SliderView(color: .blue, sliderValue: $blueValue)
+                }
+                //  Свойство isFocuced принимает значение true, благодаря тому, что отслеживает клавиатуру. После нажатия добавленной нами кнопки, клавиатура закрывается.
+                .focused($isInputActive)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            isInputActive = false
+                        }
+                    }
                 }
                 Spacer()
             }
