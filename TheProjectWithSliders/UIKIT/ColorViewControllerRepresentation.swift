33edb0e2349d90fd30c3e6710ee6baa354771b84
AtesiT <atesit@mail.ru>
2026-03-08 23:56:14 +0300

    FIX: added ignoreSafeArea and other viewController

--- TheProjectWithSliders/UIKIT/ColorViewControllerRepresentation.swift
+++ TheProjectWithSliders/UIKIT/ColorViewControllerRepresentation.swift
@@ -4,20 +4,16 @@ struct ColorViewControllerRepresentation: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         //  Возвращаем storyboard. Перед этим в Storyboard ID необходимо добавить название класса, а после этого передать его в withIdentifier
-        return storyboard.instantiateViewController(withIdentifier: "InitialViewController")
+        return storyboard.instantiateViewController(withIdentifier: "ViewController")
     }
     
     //  Оставляем пустым, так как наш ViewController независим от других
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         
     }
-    
-//      Указываем с каким UIViewController будем работать, а после добавления автоматически обязательных методов - удаляем эту строку
-//    typealias UIViewControllerType = UIViewController
-    
-    
 }
 
 #Preview {
     ColorViewControllerRepresentation()
+        .ignoresSafeArea()
 }
