cd5f203e3734226512d68ef56f6cb3005ac97ec4
AtesiT <atesit@mail.ru>
2026-03-09 19:44:41 +0300

    ADD: UITextFieldRepresentation

---
+++ TheProjectWithSliders/UIKIT/UITextFieldRepresentation.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+
+struct UITextFieldRepresentation: UIViewRepresentable {
+    func makeUIView(context: Context) -> UITextField {
+        
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        
+    }
+    
+    
+    
+    //  Определяем тип, для которого будет создана UIViewRepresentable. После чего, можем удалить этот метод или закомментировать.
+    //  typealias UIViewType = UITextField
+    
+    
+}
+
+#Preview {
+    UITextFieldRepresentation()
+}
