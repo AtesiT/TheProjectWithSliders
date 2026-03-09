46f02f13e39afc8a5cdcf8b861d7c8e0b20a55be
AtesiT <atesit@mail.ru>
2026-03-09 21:18:55 +0300

    ADD: toolbar with buttons in UITextFieldRepresentation

--- TheProjectWithSliders/UIKIT/UITextFieldRepresentation.swift
+++ TheProjectWithSliders/UIKIT/UITextFieldRepresentation.swift
@@ -3,11 +3,33 @@ import SwiftUI
 
 struct UITextFieldRepresentation: UIViewRepresentable {
     func makeUIView(context: Context) -> UITextField {
+        let toolbar = UIToolbar()
+        //  Расширяем toolbar до ширины клавиатуры
+        toolbar.sizeToFit()
+        
         let textField = UITextField()
         textField.keyboardType = .numberPad
         textField.borderStyle = .roundedRect
         textField.textAlignment = .right
-        return textField
+        textField.inputAccessoryView = toolbar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            //  Target - местоположение метода, который вызывается по нажатию на кнопку
+            target: textField,
+            //  Указываем, что нам необходимо вызвать
+            action: #selector(textField.resignFirstResponder)
+        )
+        
+        //  Создаём пустое пространство
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        //  Добавляем в toolBar
+        toolbar.items = [flexibleSpace, doneButton]
     }
     
     //  Используется для обновления значений из SwiftUI в UIKit
