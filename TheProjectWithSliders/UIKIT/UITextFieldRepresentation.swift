fac6e8bd5e9e061b62b2e7fb67b049587451e73c
AtesiT <atesit@mail.ru>
2026-03-09 20:02:21 +0300

    ADD: textField in UITextFieldRepresentation

--- TheProjectWithSliders/UIKIT/UITextFieldRepresentation.swift
+++ TheProjectWithSliders/UIKIT/UITextFieldRepresentation.swift
@@ -3,9 +3,14 @@ import SwiftUI
 
 struct UITextFieldRepresentation: UIViewRepresentable {
     func makeUIView(context: Context) -> UITextField {
-        
+        let textField = UITextField()
+        textField.keyboardType = .numberPad
+        textField.borderStyle = .roundedRect
+        textField.textAlignment = .right
+        return textField
     }
     
+    //  Используется для обновления значений из SwiftUI в UIKit
     func updateUIView(_ uiView: UITextField, context: Context) {
         
     }
